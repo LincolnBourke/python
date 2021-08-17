e6c900d5b706a42411f762261321804b2c54135d
LincolnBourke <89069393+LincolnBourke@users.noreply.github.com>
2021-08-17 18:11:42 +1000

    Update sayMyName.py

--- sayMyName.py
+++ sayMyName.py
@@ -1,1 +1,7 @@
+# program to print your name N times where N is the first two digits of your SID
+name = input("Enter your name: ")
+sid = input("Enter your SID: ")
+N = int(sid[:2])
 
+for i in range(N):
+  print(name)
